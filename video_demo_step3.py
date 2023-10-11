cbc36217be26f5081659b3013e4662514cf6ebe9
Luiz Henrique Buris <henrique.buris@outlook.com.br>
2023-10-11 08:44:38 -0300

    Add files via upload
    update code.

--- video_demo_step3.py
+++ video_demo_step3.py
@@ -4,10 +4,6 @@ import argparse
 import cv2
 import mmcv
 import torch
-
-from pathlib import Path
-import matplotlib.pyplot as plt
-from functools import reduce
 import numpy as np
 
 from ultralytics import SAM
@@ -18,7 +14,7 @@ from mmdet.apis import inference_detector, init_detector
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='MMDetection video demo step3')
+    parser = argparse.ArgumentParser(description='MMDetection video demo step1')
     parser.add_argument('video', help='Video file')
     parser.add_argument('config', help='Config file')
     parser.add_argument('checkpoint', help='Checkpoint file')
@@ -46,9 +42,6 @@ def main():
     # Load a model sam
     model_sam = SAM('sam_b.pt')
 
-    # Display model information (optional)
-    #model_sam.info()
-
     model = init_detector(args.config, args.checkpoint, device=args.device)
 
     chosen_class_id = 2 # Defina o ID da classe do objeto desejado apos olha os pessos do arquivo (por exemplo, carro)
@@ -59,6 +52,9 @@ def main():
     #('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
     print(checkpoint['meta']['CLASSES'][chosen_class_id]) ## Classe carro
 
+    Classes_ = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+
     video_reader = mmcv.VideoReader(args.video)
     video_writer = None
     if args.out:
@@ -68,13 +64,10 @@ def main():
             (video_reader.width, video_reader.height))
         
 
-    count_car = 0  # Inicializa a contagem de carros
-    bbox_list = []  # Inicializa uma lista para armazenar as BBoxes dos resultados selecionados
-
     for frame in mmcv.track_iter_progress(video_reader):
         result = inference_detector(model, frame)
+        image_copy = frame.copy()
         # Filtrar apenas as detecções do objeto desejado (carro)
-
         if isinstance(result, tuple):
             bbox_result, segm_result = result
             print(bbox_result)
@@ -84,42 +77,36 @@ def main():
             bbox_result, segm_result = result, None 
             bboxes = np.vstack(bbox_result)
 
-        labels = [np.full(res.shape[0], i, dtype=np.int32) for i, res in enumerate(result)]
-        #labels [ 0  0  0  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2 2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2 2  7  7  7 13 13 28 56 56]
-        r = [np.full(res.shape[2], i, dtype=np.int32) for i, res in enumerate(result)]
+        labels = [np.full(res.shape[0], i, dtype=np.int32) for i, res in enumerate(bbox_result)]
+        labels = np.concatenate(labels)
 
-        filtered_labels = [x for x in labels if x == chosen_class_id]
-        
-        result = [detection for detection in result[0] if detection[4] == chosen_class_id]
+        count_car = np.count_nonzero(labels == chosen_class_id)
 
-         # Armazena as BBoxes dos resultados selecionados na lista
-        bbox_list.extend([detection[:4] for detection in result])
+        for labels_, box in zip(labels,bboxes):
 
-        # Adiciona o número de detecções de carros ao contador
-        count_car += len(result)
+            if(labels_ == chosen_class_id):
+                x1, y1, x2, y2, z = map(int, box)
+                res = model_sam(frame, bboxes=box[0:4].numpy())
+                masks = res['segmentation']
+                cv2.rectangle(image_copy, (x1, y1), (x2, y2), (0, 255, 0), 1) 
+                cv2.putText(image_copy, Classes_[labels_], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-        res = model_sam(frame, bboxes=bboxes.numpy())
 
-        # Obtém as máscaras de segmentação a partir do resultado SAM na documentação do modelo tem todas as keys.
-        masks = res['segmentation']
+                # Sobreponha a máscara na imagem
+                for mask in masks:
+                    mask = np.array(mask, dtype=np.uint8)
+                    mask = cv2.resize(mask, (x2 - x1, y2 - y1))
+                    roi = image_copy[y1:y2, x1:x2]
 
-        # Adiciona sobreposições visuais das máscaras no quadro original
-        for mask in masks:
-            frame = cv2.addWeighted(frame, 1, mask, 0.5, 0) ###<<<<<<<<<<<<<<<<<<<<<==== muito lento, mas prova de conceito ok
+                    # Aplique a máscara apenas na região da caixa delimitada
+                    result = cv2.addWeighted(roi, 1, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), 0.5, 0)
+                    image_copy[y1:y2, x1:x2] = result
 
-        frame = model.show_result(frame, result, score_thr=args.score_thr)
-        if args.show:
-            cv2.namedWindow('video', 0)
-            mmcv.imshow(frame, 'video', args.wait_time)
-        if args.out:
-            video_writer.write(frame)
-
-    if video_writer:
-        video_writer.release()
+    print(f"Total de carros detectados por frame: {count_car}")
+    #cv2_imshow(image_copy)
+    cv2.imshow('Detecções Faster R-CNN', image_copy)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
-    print(f"Total de carros detectados: {count_car}")
 
 
 if __name__ == '__main__':
